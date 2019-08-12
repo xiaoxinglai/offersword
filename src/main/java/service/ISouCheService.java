66031b761fd64cb4451ed201ac7d84012f7a285e
laixiaoxing <291826425@qq.com>
2019-08-12 10:02:22 +0800

    no message

---
+++ src/main/java/service/ISouCheService.java
@@ -0,0 +1,17 @@
+package service;
+
+/**
+ * @ClassName service
+ * @Author laixiaoxing
+ * @Date 2019/4/20 上午11:59
+ * @Description TODO
+ * @Version 1.0
+ */
+public interface ISouCheService {
+
+    void soucheService();
+
+    void soucheServiceParam(Integer param);
+
+    void  test();
+}
