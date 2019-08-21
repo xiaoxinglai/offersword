9157c28e5ac4e6956ace4eff62df039abe30021a
laixiaoxing <291826425@qq.com>
2019-08-21 10:46:01 +0800

    对称二叉树 用队列判断

--- src/main/java/leetcode/对称二叉树/Solution.java
+++ src/main/java/leetcode/对称二叉树/Solution.java
@@ -1,5 +1,8 @@
 package leetcode.对称二叉树;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 给定一个二叉树，检查它是否是镜像对称的。
  * <p>
@@ -50,7 +53,7 @@ public class Solution {
             return true;
         }
 
-        if (root.left == null||root.right == null) {
+        if (root.left == null || root.right == null) {
             return false;
         }
 
@@ -69,7 +72,72 @@ public class Solution {
         return (t1.val == t2.val) && equalTree(t1.right, t2.left) && equalTree(t1.left, t2.right);
     }
 
+
+    /**
+     * 链表先序构建二叉树
+     */
+    TreeNode creat(LinkedList<Integer> linkedList) {
+
+        TreeNode treeNode = new TreeNode(linkedList.removeFirst());
+        treeNode.left = creat(linkedList);
+        treeNode.right = creat(linkedList);
+        return treeNode;
+
+    }
+
+
+    /**
+     * 思路：
+     * 如果互为镜像  那么就会出现，a的左子树等于b的右子树   a的右子树等于b的左子树
+     * 利用队列替代递归
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetricStack(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.right == null && root.left == null) {
+            return true;
+        }
+
+        if (root.left == null || root.right == null) {
+            return false;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while (!queue.isEmpty()) {
+            TreeNode t1 =  queue.poll();
+            TreeNode t2 =  queue.poll();
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+
+            if (t1.val != t2.val) {
+                return false;
+            }
+
+            queue.add(t1.left);
+            queue.add(t2.right);
+            queue.add(t1.right);
+            queue.add(t2.left);
+        }
+
+
+        return true;
+
+    }
+
     public static void main(String[] args) {
+
 
     }
 }
