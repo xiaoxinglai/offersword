092401ea72981ce240f7b2a4746ea257bd4dc107
laixiaoxing <291826425@qq.com>
2019-08-22 11:47:31 +0800

    二叉树的层序遍历 （从底开始 队列实现）

--- src/main/java/leetcode/二叉树的层次遍历/Solution.java
+++ src/main/java/leetcode/二叉树的层次遍历/Solution.java
@@ -1,6 +1,6 @@
 package leetcode.二叉树的层次遍历;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
@@ -24,8 +24,40 @@ import java.util.List;
 public class Solution {
 
 
+    /**
+     * 层次遍历 ，然后倒序
+     * 难点在于知道自己的层数 通过size记录每一层元素个数完成
+     *
+     * @param root
+     * @return
+     */
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int size = queue.size();
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = queue.poll();
+                list.add(treeNode.val);
+                if (treeNode.left != null) {
+                    queue.offer(treeNode.left);
+                }
 
+                if (treeNode.right != null) {
+                    queue.offer(treeNode.right);
+                }
+            }
+            size = queue.size();
+            result.add(list);
+        }
+
+        Collections.reverse(result);
+        return result;
     }
 
 
