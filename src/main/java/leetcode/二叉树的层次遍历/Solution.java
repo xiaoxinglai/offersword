5b0d4f24819b94ece52a7f7f5cff98b2f96c37a5
laixiaoxing <291826425@qq.com>
2019-08-22 11:38:01 +0800

    二叉树 层次遍历 知道自己遍历的层数
         思路：采取使用一个记录每层个数的一个值来判断什么时候完成遍历的。
          我们首先就应该在循环外面把根节点放入队列，然后用此时队列的大小做循环取数，并将子节点放到队列里面
          循环完之后，一层也就结束了，此时该层所有子节点也都入队了，更新下一层的每层个数

---
+++ src/main/java/leetcode/二叉树的层次遍历/Solution.java
@@ -0,0 +1,42 @@
+package leetcode.二叉树的层次遍历;
+
+import java.util.List;
+
+/**
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ * <p>
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回其自底向上的层次遍历为：
+ * <p>
+ * [
+ * [15,7],
+ * [9,20],
+ * [3]
+ * ]
+ */
+public class Solution {
+
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
