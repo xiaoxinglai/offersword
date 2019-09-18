bba49e40033b53d1187cc78c2da56aff50e0a251
laixiaoxing <291826425@qq.com>
2019-09-18 23:39:24 +0800

    no message

--- src/main/java/leetcode/最小覆盖子串/Solution.java
+++ src/main/java/leetcode/最小覆盖子串/Solution.java
@@ -16,9 +16,16 @@ import java.util.*;
  */
 public class Solution {
 
+    /**
+     * 滑动窗口
+     *
+     * @param s
+     * @param t
+     * @return
+     */
     public static String minWindow(String s, String t) {
 
-        if (t.length()>s.length()){
+        if (t.length() > s.length()) {
             return "";
         }
 
@@ -39,7 +46,7 @@ public class Solution {
         //4.然后重复3  直到右边达到最大长度
         int left = 0;
         int right = 0;
-        int min=s.length();
+        int min = s.length();
 
 
         String child = "";
@@ -59,31 +66,31 @@ public class Solution {
                 right = i;
 
                 //收缩左边
-                while (checkMap(tmap, smap)){
-                   if (smap.get(s.charAt(left))>1){
-                       smap.put(s.charAt(left),smap.get(s.charAt(left))-1);
-                   }else {
-                       smap.remove(s.charAt(left));
-                   }
-                   left++;
+                while (checkMap(tmap, smap)) {
+                    if (smap.get(s.charAt(left)) > 1) {
+                        smap.put(s.charAt(left), smap.get(s.charAt(left)) - 1);
+                    } else {
+                        smap.remove(s.charAt(left));
+                    }
+                    left++;
                 }
 
-                if (min>right-left){
-                    String s1=s.substring( left-1,right + 1);
-                    System.out.println("子串："+s1);
-                    if (child==""){
-                        child= s1;
-                    }else {
-                        if (child.length()>s1.length()){
-                            child=s1;
+                if (min > right - left) {
+                    String s1 = s.substring(left - 1, right + 1);
+                    System.out.println("子串：" + s1);
+                    if (child == "") {
+                        child = s1;
+                    } else {
+                        if (child.length() > s1.length()) {
+                            child = s1;
                         }
                     }
-                    min=right-left;
+                    min = right - left;
                 }
             }
         }
 
-        if (right==left&&left==0){
+        if (right == left && left == 0) {
             return "";
         }
         return child;
@@ -92,8 +99,8 @@ public class Solution {
 
     static boolean checkMap(HashMap<Character, Integer> tmap, HashMap<Character, Integer> smap) {
         for (Map.Entry<Character, Integer> kv : tmap.entrySet()) {
-            if (smap.get(kv.getKey())==null||smap.get(kv.getKey())<kv.getValue()){
-                return  false;
+            if (smap.get(kv.getKey()) == null || smap.get(kv.getKey()) < kv.getValue()) {
+                return false;
             }
         }
 
@@ -101,7 +108,122 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(minWindow("cabwefgewcwaefgcf", "cae"));
+        System.out.println(minWindow2("ADOBECODEBANC", "ABC"));
         //cwae
     }
+
+    static class KV {
+        private Character k;
+        private Integer v;
+
+        public KV(Character k, Integer v) {
+            this.k = k;
+            this.v = v;
+        }
+
+        public Character getK() {
+            return k;
+        }
+
+        public Integer getV() {
+            return v;
+        }
+    }
+
+    /**
+     * 带有过滤的滑动窗口 将s建立成 只包含t中字符的字符串
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public static String minWindow2(String s, String t) {
+
+        if (t.length() > s.length()) {
+            return "";
+        }
+
+
+        HashMap<Character, Integer> tmap = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            char a = t.charAt(i);
+            if (tmap.get(a) != null) {
+                tmap.put(a, tmap.get(a) + 1);
+            } else {
+                tmap.put(a, 1);
+            }
+        }
+
+
+        List<KV> list = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (tmap.get(s.charAt(i)) != null) {
+                list.add(new KV(s.charAt(i), i));
+            }
+        }
+
+
+        //1.从第一个位置开始遍历和子串字符匹配，如果不满足，则扩大窗口右边 直到满足
+        //2.记录下这个子串
+        //3.此时收缩左边，如果满足则继续收缩，直到不满足，记录下这个子串，右边继续扩大，直到满足
+        //4.然后重复3  直到右边达到最大长度
+        int left = 0;
+        int right = 0;
+        int min = s.length();
+
+        int last = 0;
+        String child = "";
+
+        LinkedHashMap<Character, Integer> smap = new LinkedHashMap<>();
+
+
+        for (int i = 0; i < list.size(); i++) {
+            KV kv = list.get(i);
+            char a = kv.getK();
+            if (smap.get(a) != null) {
+                smap.put(a, smap.get(a) + 1);
+            } else {
+                smap.put(a, 1);
+            }
+
+
+            if (checkMap(tmap, smap)) {
+                //相等 说明已经找到了满足的子串
+
+                right = kv.getV();
+                //todo 这一步还有问题
+                left = list.get(last).getV();
+
+                //收缩左边
+                while (checkMap(tmap, smap)) {
+                    if (smap.get(s.charAt(left)) > 1) {
+                        smap.put(s.charAt(left), smap.get(s.charAt(left)) - 1);
+                    } else {
+                        smap.remove(s.charAt(left));
+                    }
+                    last ++;
+                    left = list.get(last).getV();
+                }
+
+                if (min > right - left) {
+
+                    String s1 = s.substring(left , right+1 );
+                    if (child == "") {
+                        child = s1;
+                    } else {
+                        if (child.length() > s1.length()) {
+                            child = s1;
+                        }
+                    }
+                    min = right - left;
+                }
+            }
+
+        }
+
+
+        return child;
+    }
+
+
 }
